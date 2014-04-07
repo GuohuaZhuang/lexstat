1fc2437456d94e776a5fe10cefe9bd7214a4d49f
firstboy0513 <guohua0594@sina.com>
2014-04-07 14:25:00 +0800

    modify gendict shell complier of oxford and qdbm_read

--- src/gendict.sh
+++ src/gendict.sh
@@ -2,6 +2,8 @@
 
 gcc gendict.c -o gendict stemming.c -g3 -O0 -Wall -lqdbm
 gcc gendict_stopword.c -o gendict_stopword stemming.c -g3 -O0 -Wall -lqdbm
+gcc gendict_oxford.c -o gendict_oxford stemming.c -g3 -O0 -Wall -lqdbm
+gcc qdbm_read.c -o qdbm_read stemming.c -g3 -O0 -Wall -lqdbm
 
 ls ../data/*.raw | grep -v STOP | cut -d '.' -f 3 | awk '{print "./gendict .."$1".raw .."$1".DICT"}' | sh
 
